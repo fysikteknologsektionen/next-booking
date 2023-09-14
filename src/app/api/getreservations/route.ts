d00b133b25485eeb812c1a12539b3df61d9c4513
Wilmer Z <wilmerz@chalmers.se>
2023-09-14 18:43:14 +0000

    Filterable table
    Next we will try to implement menus that prefetch possible database entries for easier filtering

--- src/app/api/getreservations/route.ts
+++ src/app/api/getreservations/route.ts
@@ -4,5 +4,5 @@ import { NextResponse } from "next/server";
 
 export async function GET() {
     const reservations = await getReservationsServer();
-    return NextResponse.json({ reservations });
+    return NextResponse.json(reservations);
 }
--- src/app/list-reservations/page.tsx
+++ src/app/list-reservations/page.tsx
@@ -1,12 +1,93 @@
-import { getReservationsServer } from "@/lib/fetching";
+'use client'
+import { getReservationsClient } from "@/lib/fetching";
 import { Reservation } from "@prisma/client";
+import { useEffect, useState, useMemo } from "react";
 
-export default async function ReservationsList() {
-    const reservations = await getReservationsServer();
+export default function ReservationsList() {
+    const [searchTerms, setSearchTerms] = useState<string[]>();
+    const [reservations, setReservations] = useState<Reservation[]>();
+
+    useEffect(() => {
+        const callAsync = async () => {
+            const res = await getReservationsClient();
+            setReservations(res);
+        }
+
+        callAsync();
+    }, []);
+
+    const filteredReservations = useMemo(() => {
+        if (!searchTerms) return reservations;
+
+        if (reservations && reservations.length > 0) {
+            const list: Reservation[] = [];
+
+            for (const entry of reservations) {
+                let count: number = 0;
+                for (const searchTerm of searchTerms) {
+                    for (const value of Object.values(entry)) {
+                        if (!value) continue;
+                        if (value == searchTerm || (typeof value === 'string' && value.includes(searchTerm)))  {
+                            count += 1;
+                            break;
+                        }
+                    }
+                }
+
+                if (count === searchTerms.length) {
+                    list.push(entry);
+                }
+            }
+
+            return list;
+        }
+
+        return [];
+    }, [searchTerms, reservations]);
+
+    const renderReservation = (r: Reservation) => {
+        return (
+            <tr key={r.id}>
+                <td>{r.id}</td>
+                <td>{JSON.stringify(r.createdAt)}</td>
+                <td>{JSON.stringify(r.updatedAt)}</td>
+                <td>{r.clientName}</td>
+                <td>{r.clientEmail}</td>
+                <td>{r.clientDescription}</td>
+                <td>{JSON.stringify(r.date)}</td>
+                <td>{JSON.stringify(r.startTime)}</td>
+                <td>{JSON.stringify(r.endTime)}</td>
+                <td>{r.status}</td>
+                <td>{r.venueId}</td>
+            </tr>
+        );
+    } 
 
     return (
-        <ul>
-            {reservations.map((reservation:Reservation) => (<li key={reservation.id}>{JSON.stringify(reservation)}</li>))}
-        </ul>
+        <div>
+            <input type="text" placeholder="Type something..." onChange={(e) => setSearchTerms(e.target.value.split(','))}></input>
+            <table style={{fontSize: '12px'}}>
+                <thead>
+                    <tr>
+                        <th>Id</th>
+                        <th>Create</th>
+                        <th>Update</th>
+                        <th>CName</th>
+                        <th>CEmail</th>
+                        <th>CDesc</th>
+                        <th>Date</th>
+                        <th>Start</th>
+                        <th>End</th>
+                        <th>Status</th>
+                        <th>Venue</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {filteredReservations && filteredReservations.map((reservation: Reservation, index: number) => (
+                        renderReservation(reservation)
+                    ))}
+                </tbody>
+            </table>
+        </div>
     )
 }
