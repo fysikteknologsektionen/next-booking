9aa1a3effad686c1de0c836de4f67beef5dd9f33
Wilmer Z <wilmerz@chalmers.se>
2023-11-09 17:18:07 +0000

    Approve with collisions and deny reservations

--- src/server/api/approveReservation.ts
+++ src/server/api/approveReservation.ts
@@ -2,19 +2,41 @@ import { Status } from "@prisma/client";
 import prisma from "../lib/prisma";
 
 
-// Create a reservation, used on the server
-export async function approveReservationServer(reservationID:number) {
+// Approve a reservation, used on the server
+export async function approveReservationServer(reservationID: number) {
+    const reservation = await prisma.reservation.findUnique({
+        where: {
+            id: reservationID,
+        },
+    });
+    const collisions = await prisma.reservation.findMany({
+        where: {
+            status: Status.ACCEPTED,
+            venueId: reservation?.venueId,
+            startTime: {
+                lte: reservation?.endTime,
+            },
+            endTime: {
+                gte: reservation?.startTime,
+            },
+        },
+    });
+
+    if (collisions && collisions.length > 0) return;
+
     const result = await prisma.reservation.update({
-        where: {id: reservationID},
+        where: {
+            id: reservationID,
+        },
         data: {
             status: Status.ACCEPTED
         },
-    });;
+    });
     return result;
 }
 
 
-// Create a reservation, used on the client
+// Approve a reservation, used on the client
 export async function approveReservationClient(reservationID:number) {
     try {
         const body = { reservationID: reservationID };
