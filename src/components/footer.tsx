4d6d0e80aaaa2e3e728d06a42a92a97b7df9a53b
TheCoder5550 <alfons.nilsson@outlook.com>
2024-10-18 11:52:36 +0000

    Footer first draft

---
+++ src/components/footer.tsx
@@ -0,0 +1,25 @@
+import { Link } from "@chakra-ui/react";
+import Image from "next/image";
+
+export default function Footer() {
+    const year = new Date().getFullYear();
+
+    return (
+        <div style={{
+            backgroundColor: "hsl(0, 0%, 10%)",
+            color: "hsl(0, 0%, 60%)",
+            padding: "3rem",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+            gap: "1rem",
+            marginTop: "4rem"
+        }}>
+            <Image width="200" height="200" src="https://ftek.se/wp-content/themes/ftek-theme/images/fantomen.svg" alt="Fantomen"></Image>
+            <span>Underhåll och utveckling: <Link color="hsl(0, 0%, 80%)" href="https://ftek.se/spidera/">Spidera</Link></span>
+            <span><Link color="hsl(0, 0%, 80%)" href="https://ftek.se/kontakt/">Kontakt</Link> | <Link color="hsl(0, 0%, 80%)" href="https://ftek.se/integritetspolicy/">Integritetspolicy</Link> | <Link color="hsl(0, 0%, 80%)" href="https://ftek.se/support">Support</Link></span>
+            <span>&copy; {year} Fysikteknologsektionen</span>
+        </div>
+    )
+}
